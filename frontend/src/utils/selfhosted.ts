8352d0843f58db8471004d0747e48b9b3fa25e22
zeroshare-dev <hello@zeroshare.io>
2025-09-11 23:04:34 +0200

    Remove log

--- frontend/src/utils/selfhosted.ts
+++ frontend/src/utils/selfhosted.ts
@@ -1,6 +1,2 @@
 export const isSelfHosted = process.env.SELFHOSTED === "true";
 export const isHosted = !isSelfHosted;
-
-console.log("isSelfHosted:", isSelfHosted);
-console.log("isHosted:", isHosted);
-console.log("SELFHOSTED:", process.env.SELFHOSTED);
